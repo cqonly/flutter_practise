28e03fb41c345043364cf4eb17dd94ddadc40d73
chenqu <chenqu@znds.com>
2019-10-10 09:40:41 +0800

    main.dart，无限滚动listView,添加交互，导航到新页面，主题更改UI

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 //    final wordPair = new WordPair.random();
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-//          child: new Text(wordPair.asPascalCase),
-          child: new RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      theme: new ThemeData(primaryColor: Colors.white),
+      home: new RandomWords(),
     );
   }
 }
@@ -31,10 +24,78 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final wordPair = WordPair.random();
-    return new Text(wordPair.asPascalCase);
+
+//    final wordPair = WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Startup Name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final titles = _saved.map((pair) {
+        return new ListTile(
+            title: new Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ));
+      });
+      final divided =
+          ListTile.divideTiles(tiles: titles, context: context).toList();
+      return new Scaffold(
+        appBar: new AppBar(title: new Text("Saved Suggestions")),
+        body: new ListView(children: divided),
+      );
+    }));
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        if (index.isOdd) return new Divider();
+        final i = index ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[i]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
   }
 }
